d687aacef097139b8ebc2b4485ec2dcce85f4beb
ulfatjon <xamrokulovulfatjon2005@gmail.com>
2025-04-29 15:42:57 +0500

    The project is finished.

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import SignIn from "./pages/AuthPages/SignIn";
 import NotFound from "./pages/OtherPage/NotFound";
 import LineChart from "./pages/Charts/LineChart";
@@ -11,44 +10,39 @@ import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import Home from "./pages/Dashboard/Home";
 
-function AuthRedirect() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      navigate("/signin");
-    }
-  }, [navigate]);
-
-  return null;
-}
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        {/* Auth Layout */}
-        <Route path="/signin" element={<SignIn />} />
-
-        {/* Protected routes that require authentication */}
-        <Route element={<AuthRedirect />}>
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          {/* Dashboard Layout */}
           <Route element={<AppLayout />}>
             <Route index path="/" element={<Home />} />
+
+            {/* Others Page */}
+           
             <Route path="/blank" element={<Blank />} />
-            <Route path="/form-elements" element={<FormElements />} />
-            <Route path="/basic-tables" element={<BasicTables />} />
+
+            {/* Forms */}
+            <Route path="/books-create" element={<FormElements />} />
+
+            {/* Tables */}
+            <Route path="/category-all" element={<BasicTables />} />
+
+
+            {/* Charts */}
             <Route path="/line-chart" element={<LineChart />} />
             <Route path="/bar-chart" element={<BarChart />} />
           </Route>
-        </Route>
 
-        {/* Not Found */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+          {/* Auth Layout */}
+          <Route path="/signin" element={<SignIn />} />
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
-
-export default App;
